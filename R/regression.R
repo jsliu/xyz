98e864fc447a5341d4f0323e2726d74ee1bccf86
Zhen Liu <zhenliu@Zhens-Air.home>
2018-09-22 22:51:08 +0100

    add function ic.xyz_regression for model selection

--- R/regression.R
+++ R/regression.R
@@ -276,3 +276,45 @@ predict.xyz_regression_result<-function(object,newdata,...) {
   }
   return(Y_pred_matrix)
 }
+
+#' @export
+ic.xyz_regression<-function(x,y,crit=c("bic","aic","aicc","hqc"),...)
+{
+  if (is.matrix(x) == FALSE) {
+    x <- as.matrix(x)
+  }
+  if (is.vector(y) == FALSE) {
+    y <- as.vector(y)
+  }
+  crit<-match.arg(crit)
+  n<-length(y)
+  model <- xyz_regression(x = x, y = y, ...)
+  lambda <- model[[5]]
+  df <- sapply(model[[1]],length) + sapply(model[[3]],length)
+
+  yhat <- predict(model,x,...)
+  residuals <- (y - yhat)
+  mse <- colMeans(residuals^2)
+  sse <- colSums(residuals^2)
+  nvar <- df + 1
+  bic <- n*log(mse)+nvar*log(n)
+  aic <- n*log(mse)+2*nvar
+  aicc <- aic+(2*nvar*(nvar+1))/(n-nvar-1)
+  hqc <- n*log(mse)+2*nvar*log(log(n))
+  sst <- (n-1)*var(y)
+  r2 <- 1 - (sse/sst)
+  adjr2 <- (1 - (1 - r2) * (n - 1)/(nrow(x) - nvar - 1))
+
+  crit <- switch(crit,bic=bic,aic=aic,aicc=aicc,hqc=hqc)
+
+  selected <- best.model <- which(crit == min(crit))
+
+  ic <- c(bic=bic[selected],aic=aic[selected],aicc=aicc[selected],hqc=hqc[selected])
+
+  result <- list(best.model=selected,ic=ic,lambda=lambda[[selected]], nvar=nvar[selected],
+              glmnet=model,residuals=residuals[,selected],fitted.values=yhat[,selected],
+              ic.range=crit, call = match.call())
+
+  class(result) <- "ic.xyz_regression"
+  return(result)
+}
