951b2859744aa6a4baab93bd5467ce8a76876085
Zhen Liu <jasonliu1981@gmail.com>
2018-10-10 16:05:54 +0100

    bug fixed

--- src/core.cpp
+++ src/core.cpp
@@ -522,6 +522,7 @@ List projected_equal_pairs(IntegerMatrix X, NumericVector Y, int number_of_runs,
     //if(s2 > 0.9) {Rcout << "s2=" << s2 <<" "; stop("background interaction strength seems to be unusually high");}
     //if(s2 < 0.1) {Rcout << "s2=" << s2 <<" "; stop("background interaction strength seems to be unusually low");}
     if (s2 < 0.1 || s2 > 0.9) {
+        Rcout << "s2=" << s2;
         return List();
     }
 
@@ -641,9 +642,10 @@ List interaction_search(NumericMatrix X, NumericVector Y, NumericVector weights,
     }
     int max_number_of_collisions = 2*p;
     List pairs = projected_equal_pairs(X_binary, Y,number_of_runs, max_number_of_collisions, negative);
-    if (pairs.size() > 0) {
-        result = find_strongest_pairs(pairs,X,Y,weights,max_number_of_pairs);
+    if (pairs.size() == 0) {
+        return List();
     }
+    result = find_strongest_pairs(pairs,X,Y,weights,max_number_of_pairs);    
   }
   return result;
 }
